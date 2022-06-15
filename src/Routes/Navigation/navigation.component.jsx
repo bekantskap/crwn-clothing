468a39fb88378ef89b34dc0dbfa66d00ab36e667
Alexander Stenberg <alstenberg@live.se>
2022-06-15 10:31:45 +0200

    started working on the context

--- src/Routes/Navigation/navigation.component.jsx
+++ src/Routes/Navigation/navigation.component.jsx
@@ -1,9 +1,11 @@
 import { Link, Outlet } from 'react-router-dom';
-import { Fragment } from 'react';
+import { Fragment, useContext } from 'react';
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import './navigation.styles.scss';
+import {UserContext} from '../../contexts/user.context'
 
 const Navigation = () => {
+  const { currentUser } = useContext(UserContext);
   return (
     <Fragment>
       <nav className="navigation">
